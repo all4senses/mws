28a8ccdfb5c164ab7bba20716e5a1d797a796b37
all4senses <all4senses@gmail.com>
2012-07-28 23:26:43 +0300

    Fb

--- themes/framework/html.tpl.php
+++ themes/framework/html.tpl.php
@@ -7,6 +7,8 @@
   <?php print $head; ?>
   <!-- Set the viewport width to device width for mobile -->
   <meta name="viewport" content="width=device-width" />
+  <meta property="fb:app_id" content="271416119626001"/>
+  
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
   <?php print $scripts; ?>
