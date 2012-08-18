075afca602d84d4410a621df7c56f758980c2571
all4senses <all4senses@gmail.com>
2012-08-18 22:29:54 +0300

    Commerce

--- themes/framework/Templates/node--dept.tpl.php
+++ themes/framework/Templates/node--dept.tpl.php
@@ -126,7 +126,7 @@ if ($tids) {
   }
   
   $sort_options = array('title', 'price', 'date');
-  $start_url = $_SERVER['REDIRECT_URI'] . $query . ($query ? '&' : '?');
+  $start_url = $_SERVER['REDIRECT_URL'] . $query . ($query ? '&' : '?');
   
   $sort_exposed = '';
   
