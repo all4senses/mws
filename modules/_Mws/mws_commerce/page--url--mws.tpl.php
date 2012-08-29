487b23d2b90346093578237247ce69413d20f512
all4senses <all4senses@gmail.com>
2012-08-29 08:09:44 +0300

    Mws

--- modules/_Mws/mws_commerce/page--url--mws.tpl.php
+++ modules/_Mws/mws_commerce/page--url--mws.tpl.php
@@ -1,6 +1,6 @@
 <div id="mws" class="clearfix">
 
-   <header id="header" role="banner" class="clearfix">
+   <header id="header-mws" role="banner" class="clearfix">
 
     <?php print render($page['header']); ?>
       <nav id="navigation" role="navigation" class="clearfix">
@@ -10,15 +10,13 @@
 
   <section id="main-mws" role="main" class="clearfix">
     <?php print $messages; ?>
-    <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+    <?php if ($title): ?><div class="title" id="page-title"><?php print $title; ?></div><?php endif; ?>
     <?php if (!empty($tabs['#primary'])): ?><div class="tabs-wrapper clearfix"><?php print render($tabs); ?></div><?php endif; ?>
     <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
     <?php print render($page['content']); ?>
   </section> <!-- /#main -->
   
-
-  <footer id="footer" role="contentinfo" class="clearfix">
-    <?php print render($page['footer']) ?>
+  <footer id="footer-mws" role="contentinfo" class="clearfix">
   </footer> <!-- /#footer -->
 
 </div> <!-- /#container --> 
