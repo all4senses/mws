934a7c1785dc9eef0b49cde8436a7800e47e82ed
all4senses <all4senses@gmail.com>
2012-08-29 08:00:21 +0300

    Mws

--- modules/_Mws/mws_commerce/page--url--mws.tpl.php
+++ modules/_Mws/mws_commerce/page--url--mws.tpl.php
@@ -1,36 +1,16 @@
-<div id="container" class="clearfix">
+<div id="mws" class="clearfix">
 
    <header id="header" role="banner" class="clearfix">
-	<?php if ($logo): ?>
-      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" id="logo">
-        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-      </a>
-    <?php endif; ?>
-    <?php if ($site_name || $site_slogan): ?>
-      <hgroup id="site-name-slogan">
-        <?php if ($site_name): ?>
-          <h1 id="site-name">
-            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><span><?php print $site_name; ?></span></a>
-          </h1>
-        <?php endif; ?>
-        <?php if ($site_slogan): ?>
-          <h2 id="site-slogan"><?php print $site_slogan; ?></h2>
-        <?php endif; ?>
-      </hgroup>
-    <?php endif; ?>
 
     <?php print render($page['header']); ?>
       <nav id="navigation" role="navigation" class="clearfix">
       </nav> <!-- /#navigation -->
-    <?php if ($breadcrumb): print $breadcrumb; endif;?>
+    <?php //if ($breadcrumb): print $breadcrumb; endif;?>
   </header> <!-- /#header -->
 
-  <section id="main" role="main" class="clearfix">
+  <section id="main-mws" role="main" class="clearfix">
     <?php print $messages; ?>
-    <a id="main-content"></a>
-    <?php print render($title_prefix); ?>
     <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
-    <?php print render($title_suffix); ?>
     <?php if (!empty($tabs['#primary'])): ?><div class="tabs-wrapper clearfix"><?php print render($tabs); ?></div><?php endif; ?>
     <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
     <?php print render($page['content']); ?>
