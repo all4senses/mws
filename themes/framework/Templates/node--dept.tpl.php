cb32b163f450729b0f028a8998ef6a1d64bb502c
all4senses <all4senses@gmail.com>
2012-09-03 08:39:00 +0300

    Commerce

--- themes/framework/Templates/node--dept.tpl.php
+++ themes/framework/Templates/node--dept.tpl.php
@@ -47,7 +47,8 @@
     
 dpm($node);
 
-$term_children = taxonomy_get_children($node->field_category['und'][0]['taxonomy_term']->tid, $node->field_category['und'][0]['taxonomy_term']->vid);
+//$term_children = taxonomy_get_children($node->field_category['und'][0]['taxonomy_term']->tid, $node->field_category['und'][0]['taxonomy_term']->vid);
+$term_children = taxonomy_get_children($node->field_category['und'][0]['tid']);//, $node->field_category['und'][0]['taxonomy_term']->vid);
 
 
 $tids = NULL;
